e5ccd00cdb351e38b496967ffb249451bc8b5cf3
JoshuaBeard <beardj1@seattleu.edu>
2017-04-29 13:12:56 -0700

    updates

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,11 @@ cd ~/code/hesaff
 ./unix_build.sh
 cp build/libhesaff.so ~/code/hotspotter/hstpl/extern_feat/
 
+# build flann... yum
+cd ~/code/flann
+./unix_build.sh
+
+# Go back to the parent dir
+cd ~/code
+
 zenity --info --text='All done rebuilding and moving hesaff.'
